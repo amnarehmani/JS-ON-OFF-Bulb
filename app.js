e95e555c4d97d32aa1eaeae5e335d7850de9f6c1
amnarehmani <jeonamnaaa@gmail.com>
2024-08-23 22:07:34 +0500

    bulb project

---
+++ app.js
@@ -0,0 +1,8 @@
+function bulbOn(){
+    let bulb = document.querySelector(".bulb-off")
+    bulb.setAttribute("src", "images/onbulb-removebg-preview.png")
+}
+function bulbOff(){
+    let bulb = document.querySelector(".bulb-off")
+    bulb.setAttribute("src", "images/images__1_-removebg-preview.png")
+}
